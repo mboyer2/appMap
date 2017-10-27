400eff6b88f868c554e666f44778a7a5d1de2f86
Michael  Boyer <michaelboyer@Michaels-MacBook-Pro.local>
2017-10-27 15:51:54 -0600

    complete, need to edit <p> so its further from dot and a placeholder for input

--- appMap.js
+++ appMap.js
@@ -1,39 +1,42 @@
+
+
 var mainVm = new Vue({
 
 	el: '#app',
 	data: {
-		dots: [
+		dots: [ //pushedDot
 		],
-		notes: [
-		],
+		
 
 
 
 	},
 	methods:{
 		makeDot: function(event){
-			// console.log(event)
+			
 			// console.log(event.offsetX, event.offsetY)
 			this.dots.push({
 				pageX: event.offsetX+7 + 'px',
 				pageY: event.offsetY-8 + 'px',
+				message: '',
+				isBeingEdited: true,
 			})
-
-			prompt('Add notes about your location')
 			
-			this.notes.push({
-				notesX: event.offset+15 + 'px',
-				notesY: event.offset-16 + 'px',
-
-			})
 		
 		},
+
+		submit: function(dot){
+			console.log(dot)
+			dot.isBeingEdited = false
+
+
+		},
+
 			
 		
 		removeDot: function(dot, index){
-			// console.log(index)
+			
 			this.dots.splice(index, 1)
-
 			// for (var i = 0; i < this.dots.length; i++ ){
 			// 	if(dot.pageX === this.dots[i].pageX && dot.pageY === this.dots[i].pageY){
 					//this.dots.splice([i], 1)
@@ -42,7 +45,13 @@ var mainVm = new Vue({
 
 			// }
 
-		}
+		},
+
+		backToInput: function(dot){
+			console.log(dot)
+			dot.isBeingEdited = true
+			
+		},
 
 	}
 
